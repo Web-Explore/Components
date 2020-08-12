0a8be1b9b1c8acf80ba01940f2b17412e577618b
Pavel Zubkov <pzubkov@aspirity.com>
2020-08-12 11:14:05 +0700

    Add Input

---
+++ src/components/input/input.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { cn } from '@bem-react/classname'
+import t from 'prop-types'
+import './input.css'
+
+const Types = {
+  text: 'text',
+  password: 'password',
+}
+
+const input = cn('Input')
+
+export const Input = (p) => {
+  const {
+    value,
+    onChange,
+    placeholder,
+    ...mods
+  } = p
+  
+  return (
+    <input className={input({ ...mods })}
+      value={value}
+      onChange={e => onChange(e.target.value)}
+      type={p.type}
+      placeholder={placeholder}
+      disabled={p.disabled}
+    />
+  )
+}
+
+Input.propTypes = {
+  value: t.string.isRequired,
+  onChange: t.func.isRequired,
+  
+  required: t.bool,
+  optional: t.bool,
+  disabled: t.bool,
+  
+  type: t.oneOf(Object.values(Types)),
+  placeholder: t.string,
+}
+
+Input.defaultProps = {
+  required: false,
+  optional: true,
+  disabled: false,
+  placeholder: 'Hint text',
+}
+

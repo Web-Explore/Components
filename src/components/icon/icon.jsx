af8bfcf0ccd16a146c40c34d962aac353dc8227b
Ivan Bitkov <mribitkov@gmail.com>
2020-08-19 17:17:21 +0700

    Completed

--- src/components/icon/icon.jsx
+++ src/components/icon/icon.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import {IconTemp} from './IconTemp.jsx'
-import DefaultIcon from '../../assets/icons/default-icon.svg'
-import Logout from './icons/Logout.svg';
+import Logout from './icons/Logout.svg'
+
 
 const Icon = [
   { Logout },
 ].reduce((acc,icon)=>{
   const [e] =Object.entries(icon);
-  debugger
 return { ...acc, [e[0]]: (props) => <IconTemp {...props} src={e[1]} /> };
 },{});
-//
-console.log(Icon);
-console.log(Icon.Logout);
+
 export default Icon;
